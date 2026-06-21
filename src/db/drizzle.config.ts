9679368247223059b534d7e6fd8849e255a48431
Saikat Mallick <74015966+saikatmallick@users.noreply.github.com>
2026-06-21 14:33:10 +0530

    Allow fallback for SQL user and password environment vars

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 
 const sqlHost = process.env.SQL_HOST;
 const sqlDbName = process.env.SQL_DB_NAME;
-const user = process.env.SQL_ADMIN_USER;
-const password = process.env.SQL_ADMIN_PASSWORD;
+const user = process.env.SQL_ADMIN_USER || process.env.SQL_USER;
+const password = process.env.SQL_ADMIN_PASSWORD || process.env.SQL_PASSWORD;
 
 if (!sqlHost) {
   throw new Error("SQL_HOST must be set in environment variables.");
@@ -16,10 +16,10 @@ if (!sqlDbName) {
   throw new Error("SQL_DB_NAME must be set in environment variables.");
 }
 if (!user) {
-  throw new Error("SQL_ADMIN_USER must be set in environment variables.");
+  throw new Error("SQL_ADMIN_USER or SQL_USER must be set in environment variables.");
 }
 if (!password) {
-  throw new Error("SQL_ADMIN_PASSWORD must be set in environment variables.");
+  throw new Error("SQL_ADMIN_PASSWORD or SQL_PASSWORD must be set in environment variables.");
 }
 
 export default defineConfig({
